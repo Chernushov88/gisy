4a3e59fee35f251c61496fc58e976262ab8d7852
sergey <chernushov88@mail.ru>
2015-10-19 19:47:51 +0300

    1.gisy

---
+++ is/myscript.js
@@ -0,0 +1,36 @@
+﻿/*-----------------------------------------------------------------------------------*/
+/*  scrollTop button
+/*-----------------------------------------------------------------------------------*/
+ $(document).ready(function(){   
+      //fade in/out based on scrollTop value
+      $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+          $('#ubar').fadeIn();
+        } else {
+          $('#ubar').fadeOut();
+        }
+      });
+     
+      // scroll body to 0px on click
+      $('#ubar').click(function () {
+        $('body,html').animate({
+          scrollTop: 0
+        }, 400);
+        return false;
+      });
+    });
+/*-----------------------------------------------------------------------------------*/
+/*  PopUp
+/*-----------------------------------------------------------------------------------*/
+ //Функция отображения PopUp
+      function PopUpShow(){
+        $(".darken").fadeIn();
+        $("#popup").fadeIn();
+        }
+        
+        //Функция скрытия PopUp
+        function PopUpHide(){
+        $(".darken").fadeOut(300);
+        $("#popup").fadeOut(300); 
+        }
+         
